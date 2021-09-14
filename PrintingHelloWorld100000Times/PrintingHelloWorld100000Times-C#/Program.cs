7aab4bac3d8a9f8136ece8bcb3272426264fa523
Fahad Mullaji <fahadmullaji@gmail.com>
2021-09-14 15:09:17 -0400

    printing hello world 100K times C# vs Java

---
+++ PrintingHelloWorld100000Times/PrintingHelloWorld100000Times-C#/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Printing100000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime startTime = DateTime.Now;
+            for (int i = 0; i < 100000; i++)
+            {
+                Console.WriteLine("Hello World!");
+            }
+            
+            DateTime endTime = DateTime.Now;
+            System.Console.WriteLine((endTime - startTime).Seconds);
+        }
+    }
+}
